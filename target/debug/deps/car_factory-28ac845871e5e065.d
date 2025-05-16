7dbb1762adcba70fee2a296e67aac1a353bc4e49
hello world <joshuaburge507@gmail.com>
2025-05-16 15:35:15 +0930

    committing target folder files

---
+++ target/debug/deps/car_factory-28ac845871e5e065.d
@@ -0,0 +1,6 @@
+/Users/Joshua/GitHub/car-factory/target/debug/deps/libcar_factory-28ac845871e5e065.rmeta: src/main.rs src/car.rs
+
+/Users/Joshua/GitHub/car-factory/target/debug/deps/car_factory-28ac845871e5e065.d: src/main.rs src/car.rs
+
+src/main.rs:
+src/car.rs:
---
+++ target/debug/deps/car_factory-670cadfd6f7e21e6.d
@@ -0,0 +1,6 @@
+/Users/Joshua/GitHub/car-factory/target/debug/deps/libcar_factory-670cadfd6f7e21e6.rmeta: src/main.rs src/car.rs
+
+/Users/Joshua/GitHub/car-factory/target/debug/deps/car_factory-670cadfd6f7e21e6.d: src/main.rs src/car.rs
+
+src/main.rs:
+src/car.rs:
